1da0e8b955d2b5f13bbd3e81dc223fcc653eb010
pviniciusm <pvinics@gmail.com>
2021-03-01 23:10:13 -0300

    Setting an exception default message

--- src/messages/Exception.ts
+++ src/messages/Exception.ts
@@ -2,7 +2,7 @@ import Return from './Return';
 
 class Exception extends Return {
   constructor (message: string, code?: number, identifier?: string) {
-    super(false, code || 500, message, undefined, true, identifier);
+    super(false, code || 500, message || 'An exception was thrown, please contact the admin.', undefined, true, identifier);
   }
 }
 
