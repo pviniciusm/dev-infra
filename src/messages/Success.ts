e92b5ae4eeb127e55d0f9d11a729bf1ab2342b04
pviniciusm <pvinics@gmail.com>
2021-03-01 23:07:44 -0300

    Fixing Success default message language

--- src/messages/Success.ts
+++ src/messages/Success.ts
@@ -2,7 +2,7 @@ import Return from './Return';
 
 class Success extends Return {
   constructor (data: any, message?: string, code?: number) {
-    super(true, code || 200, message || 'Operação realizada com sucesso.', data, false);
+    super(true, code || 200, message || 'Operation was successfully done.', data, false);
   }
 }
 
