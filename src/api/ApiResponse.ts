e40ab711019be6a54587e4cd114ed66b75631af5
pviniciusm <pvinics@gmail.com>
2021-03-01 23:05:18 -0300

    Switching ApiResponde status code to the return data code

--- src/api/ApiResponse.ts
+++ src/api/ApiResponse.ts
@@ -2,16 +2,16 @@ import { Response } from 'express';
 import Return from '../messages/Return';
 
 class ApiResponse {
-  static success (res: Response, data: Return, code?: number) {
-    return res.status(code || 200).send(data);
+  static success (res: Response, data: Return) {
+    return res.status(data.code || 200).send(data);
   }
 
-  static exception (res: Response, data: Return, code?: number) {
-    return res.status(code || 500).send(data);
+  static exception (res: Response, data: Return) {
+    return res.status(data.code || 500).send(data);
   }
 
-  static failure (res: Response, data: Return, code?: number) {
-    return res.status(code || 400).send(data);
+  static failure (res: Response, data: Return) {
+    return res.status(data.code || 400).send(data);
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@ app.use(express.json());
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  Infra.ApiResponse.failure(res, new Infra.RequiredFieldError('Password'), 404);
+  Infra.ApiResponse.failure(res, new Infra.InexistentEntryError('Password'));
 });
 
 router.get('/teste', (req, res: Response) => {
-  Infra.ApiResponse.success(res, new Infra.Success('Opa!', 'Operação realizada com sucesso'), 201);
+  Infra.ApiResponse.success(res, new Infra.Success('Opa!', 'Operação realizada com sucesso'));
 });
 
 app.use(router);
